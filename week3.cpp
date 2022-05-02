17508ee5e282517d4be8f8a37d6969bc7e910192
Sourik <73734325+sourik10@users.noreply.github.com>
2022-05-02 11:58:14 +0530

    Update week3.cpp

--- week3.cpp
+++ week3.cpp
@@ -1,3 +1,4 @@
+//1.Binary to Octal Converter
 #include<bits/stdc++.h>
 #define ll              long long int 
 #define ull             unsigned long long
@@ -28,3 +29,77 @@ int main(){
 	int n;cin>>n;
 	cout<<binary2Octal(n)<<endl;
 }
+
+
+//2
+#include<bits/stdc++.h>
+#define ll              long long int 
+#define ull             unsigned long long
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+
+int main(){
+	int n;cin>>n;
+	int ar[1000];
+	for(int i=0;i<n;i++){
+	    cin>>ar[i];
+	}
+	sort(ar,ar+n/2); 	//sort in ascending order
+	sort(ar+n/2, ar + n, greater<int>()); 	//sort in descending order
+	for(int i=0;i<n;i++){
+	    cout<<ar[i]<<",";
+	}
+	
+}
+
+//3
+#include<bits/stdc++.h>
+#define ll              long long int 
+#define ull             unsigned long long
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+
+bool checkPrime(int n){
+    if(n==0 || n==1) return false;
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+int main(){
+	int t;cin>>t;
+	while(t--){
+	    int s=0;
+	    int a,b;cin>>a>>b;
+	    for(int i=a;i<=b;i++){
+	        if(checkPrime(i)) s+=i;
+	    }
+	    cout<<s<<endl;
+	}
+	
+	
+}
+
+
+//4.Remove duplicate character from a string
+#include<bits/stdc++.h>
+#define ll              long long int 
+#define ull             unsigned long long
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+
+int main(){
+	string s;cin>>s;
+	unordered_set<char>st;
+	for(int i=0;i<s.length();i++){
+	    st.insert(s[i]);
+	}
+	string ans="";
+	for(auto x:st) ans+=x;
+	for(char i=ans.length()-1;i>=0;i--) cout<<ans[i];
+}
+
+
+//5.
