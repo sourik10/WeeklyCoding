e47ebba9a72d8d6948fba8c41d755bbf5cdcfc65
Sourik <73734325+sourik10@users.noreply.github.com>
2022-05-03 11:12:22 +0530

    Update week4.cpp

--- week4.cpp
+++ week4.cpp
@@ -39,3 +39,44 @@ int main(){
 		}
 	}
 }
+
+
+//2.Rearrange an array in order – smallest, largest, 2nd smallest, 2nd largest
+//https://www.geeksforgeeks.org/rearrange-array-order-smallest-largest-2nd-smallest-2nd-largest/
+//time = Time Complexity : O(n Log n) 
+//Auxiliary Space : O(n)
+
+#include<bits/stdc++.h>
+#define ll              long long int 
+#define ull             unsigned long long
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+void rearrange(int*ar,int n){
+	int temp[1000];
+	sort(ar,ar+n);
+	int arIndex=0;
+	for(int i=0,j=n-1;i<=n/2||j>n/2;i++,j--){
+		temp[arIndex]=ar[i];
+		arIndex++;
+		temp[arIndex]=ar[j];
+		arIndex++;
+	}
+	for(int i=0;i<n;i++){
+		ar[i]=temp[i];
+	}
+}
+int main(){
+	int n;cin>>n;
+	int ar[1000];
+	for(int i=0;i<n;i++){
+		cin>>ar[i];
+	}
+	rearrange(ar,n);
+	for(int i=0;i<n;i++){
+		cout<<ar[i]<<",";
+	}
+
+}
+
+
+//3
